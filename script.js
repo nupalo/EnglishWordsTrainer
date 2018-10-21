1588bf4890b5ca364b24d235a3a1995918c8d2b1
Teacher <test@test.ru>
2018-10-21 22:05:29 +0300

    Добавлена функция для показа слова в виде кнопок

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let dictionary = [
 	"apple", "word", "coffee", "green" , "red", "blue", "awesome", "amazing", "function", "artist", "singer", "black",
 	"five", "for", "about", "what", "restaurant", "silence", "hotel", "luxury", "woman", "cat", "dog", "sister", "mother",
@@ -16,4 +18,37 @@ let shuffle = (array) => {
 };
 
 shuffle(dictionary);
+
+window.addEventListener("load", function () {
+    const limitQuestions = 5;
+    let currentQuestion = 1;
+
+    let answerContainer = document.querySelector("#answer");
+    let letters = document.querySelector("#letters");
+    let currentQuestionElem = document.querySelector("#current_question");
+    let totalQuestionsElem = document.querySelector("#total_questions");
+
+    currentQuestionElem.innerHTML = currentQuestion;
+    totalQuestionsElem.innerHTML = limitQuestions;
+
+    // функция для показа слова, разбитого на буквы в кнопках
+    let displayCurrentQuestion = () => {
+    	let word = dictionary[currentQuestion];
+    	let wordArr = word.split("");
+        shuffle(wordArr);
+
+        for (let letter of wordArr) {
+        	let btn = document.createElement("button");
+        	btn.innerHTML = letter;
+        	btn.className = "btn btn-primary btn-sm mr-1";
+
+            letters.appendChild(btn);
+		}
+	};
+
+    displayCurrentQuestion();
+
+});
+
+
 
